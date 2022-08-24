10107b3b964d84d0f9a43c564eab07038466b0cb
Wojciech Jaskula <w.jaskula8@gmail.com>
2022-08-24 13:22:47 +0200

    Functionality + RWD

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,4 @@
-console.log("Oddzielny plik JavaScript");
-
 function popupFunction() {
-  // alert("popupFunction została wywołana");
-  console.log("test przycisku popup");
-
   var element = document.getElementById("myContainer");
   element.classList.toggle("shadow");
   var popup = document.getElementById("myPopup");
@@ -11,9 +6,34 @@ function popupFunction() {
 }
 
 function closeFunction() {
-  console.log("test zamykania");
   var element = document.getElementById("myContainer");
   element.classList.remove("shadow");
   var popup = document.getElementById("myPopup");
   popup.classList.remove("show");
 }
+
+let counter = 0;
+
+const spanCounter = document.querySelector("span.counter");
+const btn = document.getElementById("clickButton");
+const btnReset = document.getElementById("reset");
+
+function btnClick() {
+  counter++;
+  spanCounter.textContent = counter;
+  if (counter >= 5) {
+    btn.removeEventListener("click", btnClick);
+    const resetBtn = document.getElementById("reset");
+    resetBtn.classList.remove("reset");
+  }
+}
+
+function resetFunction() {
+  counter--;
+  spanCounter.textContent = 0;
+  const resetBtn = document.getElementById("reset");
+  resetBtn.classList.add("reset");
+}
+
+btnReset.addEventListener("click", resetFunction);
+btn.addEventListener("click", btnClick);
