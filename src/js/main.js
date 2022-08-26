1c9e01cc984134c98d3d5446ed350bcd6ddb8593
Filip Obracaj <filobr@fideltronik.int>
2022-08-26 23:06:37 +0200

    [2] - add text and button to module

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 const imagePath =
   "/images/sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_c_scale,w_1400.jpg";
 
-createModule(imagePath);
+const titleText = "Lorem Ipsum";
+const descriptionText = `Infinitely scalable, feature-rich and cloud-native data management and protection for modern and legacy infrastructures and SaaS platforms, managed via a single app with no hardware required.`;
+const buttonText = "Button";
+
+createModule(imagePath, titleText, descriptionText, buttonText);
