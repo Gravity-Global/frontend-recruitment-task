b16af54a6ffe83ff65fab3a2fd2ee7f410fbe1ea
Agnieszka Uzarek <agnieszka.czerebak@gmail.com>
2022-08-28 11:21:59 +0200

    Added functionalities in JavaScript

---
+++ src/js/main.js
@@ -0,0 +1,47 @@
+const showBtn = document.querySelector(".first-section__btn");
+const countDisplay = document.querySelector(".count-display");
+const popUpWindow = document.querySelector(".pop-up");
+const popUpCloseBtn = document.querySelector(".pop-up__close-btn");
+const popUpCloseOutside = document.querySelector(".pop-up__background");
+const popUpResetBtn = document.querySelector(".pop-up__reset-btn");
+
+let clickCounter = localStorage.getItem("clickCounter") || 0;
+
+const countUp = () => {
+  clickCounter++;
+  countDisplay.innerHTML = clickCounter;
+  localStorage.setItem("clickCounter", clickCounter);
+}
+
+const openPopUp = () => {
+  popUpWindow.style.display = 'block';
+}
+
+const closePopUp = () => {
+  popUpWindow.style.display = 'none';
+}
+
+const resetCounter = () => {
+  localStorage.clear();
+  clickCounter = 0;
+  countDisplay.innerHTML = clickCounter;
+  closePopUp();
+}
+
+showBtn.addEventListener("click", () => {
+  countUp();
+  openPopUp();
+  popUpResetBtn.style.display = (clickCounter <= 5) ? 'none' : 'block';
+});
+
+popUpCloseBtn.addEventListener("click", () => {
+  closePopUp();
+})
+
+popUpCloseOutside.addEventListener("click", () => {
+  closePopUp();
+})
+
+popUpResetBtn.addEventListener("click", () => {
+  resetCounter();
+})
